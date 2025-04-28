29947071c1e3191eab762605f100d6ff14ad639e
taeung515 <xodndd515@gmail.com>
2025-04-28 09:38:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long num1 = num;
+        
+        if(num1 == 1){
+            return 0;
+        }
+        
+        while (num1 != 1){
+            
+            if(num1 % 2 == 0){
+                num1 /= 2;
+            }else{
+                num1 = num1 * 3 + 1;
+            }
+            answer ++;
+            
+            if(answer > 500){
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
