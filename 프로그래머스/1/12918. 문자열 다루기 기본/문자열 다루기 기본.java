d31d6757c82124adbf58f3c8b224d3d571b00e9f
taeung515 <xodndd515@gmail.com>
2025-05-01 09:26:40 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        if(s.length() == 4 || s.length() == 6){
+            answer = s.matches("\\d+");
+        }
+        return answer;
+    }
+}
